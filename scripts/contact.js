f83b05021e0c1178a39776666878db46b9d020e7
Ozmar Cota <ozcota96@gmail.com>
2023-03-04 13:02:34 -0600

    Updated to bootstrap 5.3. Added alert.

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,32 +1,15 @@
-function generateMessageTemplate(){
-    var firstName = document.getElementById('first-name').value;
-    var lastName = document.getElementById('last-name').value;
-    var subject = document.getElementById('subject').value;
-    var message = document.getElementById('message-body').value;
+const contactAlertPlaceholder = document.getElementById('contact-alert');
 
-    var messageTemplate = 
-    `Nuevo mensaje de contacto:\n
-    De: ${firstName} ${lastName}\n
-    Asunto: ${subject}\n
-    Mensaje: ${message}`;
-
-    return messageTemplate;
-}
-
-function sendEmail() {
-   const request = new XMLHttpRequest();
-   
-   request.open('GET', 'http://127.0.0.1:8000/contact/', true);
-   request.send();
-
-   request.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-        alert(request.responseText);
-    }
-    else{
-        alert(request.status);
-    }
-   }
+const alert = (message, type) => {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        `   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`,
+        `</div>`
+    ].join('');
+    contactAlertPlaceholder.innerHTML = '';
+    contactAlertPlaceholder.append(wrapper);
 }
 
 function validateForm(){
@@ -35,17 +18,53 @@ function validateForm(){
 
     Array.from(forms).forEach(form => {
         form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            else{
+                //sendEmail();
+            }
+        
             event.preventDefault();
-            event.stopPropagation();
-        }
-        else{
-            sendEmail();
-        }
-    
-        form.classList.add('was-validated')
+
+            alert('Ups! Algo salió mal. Estamos trabajando en ello.', 'danger');
+
+            form.classList.add('was-validated');
         }, false);
 
         return;
     });
 }
+
+// Enable back when contact page is functional
+// function generateMessageTemplate(){
+//     var firstName = document.getElementById('first-name').value;
+//     var lastName = document.getElementById('last-name').value;
+//     var subject = document.getElementById('subject').value;
+//     var message = document.getElementById('message-body').value;
+
+//     var messageTemplate = 
+//     `Nuevo mensaje de contacto:\n
+//     De: ${firstName} ${lastName}\n
+//     Asunto: ${subject}\n
+//     Mensaje: ${message}`;
+
+//     return messageTemplate;
+// }
+
+// function sendEmail() {
+//    const request = new XMLHttpRequest();
+   
+//    request.open('GET', 'http://127.0.0.1:8000/contact/', true);
+//    request.send();
+
+//    request.onreadystatechange = function(){
+//     if(this.readyState == 4 && this.status == 200){
+//         alert(request.responseText);
+//     }
+//     else{
+//         alert(request.status);
+//     }
+//    }
+// }
