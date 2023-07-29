4b616a0f79968465253a056276069f7f2d5d1475
Osvaldo <osvaldoorozcorod@gmail.com>
2023-07-29 13:32:30 -0600

    Primer commit

---
+++ celuma-webapi/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.2"
+	id("io.spring.dependency-management") version "1.1.2"
+}
+
+group = "com.celuma"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.data:spring-data-jpa")
+
+	runtimeOnly("org.postgresql:postgresql")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
