7aabd92ec0faaf2af0c50a86ec8b6bcdafa59018
Osvaldo <osvaldoorozcorod@gmail.com>
2023-08-05 10:25:55 -0600

    Segundo commit

---
+++ celuma-webapi/src/main/java/com/celuma/webapi/CelumaWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.celuma.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelumaWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelumaWebapiApplication.class, args);
+	}
+
+}
---
+++ celuma-webapi/src/test/java/com/celuma/webapi/CelumaWebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celuma.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelumaWebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
