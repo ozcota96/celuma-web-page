1b5b189769bd2ccd6095b81797770bf7c01f66b0
Osvaldo <osvaldoorozcorod@gmail.com>
2024-02-13 19:17:42 -0600

    Se pueden agregar productos y eliminar

--- scripts/products.js
+++ scripts/products.js
@@ -13,27 +13,61 @@ function getProducts() {
         if (Http.readyState == 4) {
             if (Http.status === 200) {
                 var products = JSON.parse(Http.responseText);
-                console.log(products)
                 for (const product of products) {
                     
+                    // Row creation
                     const tableRow = document.createElement("tr")
                     productsTable.appendChild(tableRow);
 
+                    // Name display
                     const name = document.createElement("td");
                     name.textContent = product.name;
+                    // ProductId display
                     const id = document.createElement("td");
                     id.textContent = product.productId;
+
+                    // Status display
                     const status = document.createElement("td");
                     status.textContent = product.active;
+
+                    // Delete button
+                    const buttons = document.createElement("td");
+                    var button_del = createButton(product.productId, "delete");
+                    buttons.append(button_del);
+                    button_del.addEventListener("click", function() {
+                        var valueHolder = document.getElementById("button_" + product.productId)
+                        const deletedText = document.getElementById("eliminarBtn");
+                        deletedText.value = valueHolder.value;
+                    });
 
                     tableRow.appendChild(name);
                     tableRow.appendChild(id);
                     tableRow.appendChild(status);
+                    tableRow.appendChild(buttons);
+
+
                 }
             }
         }
     }
 
+}
+
+function createButton(productId, type){
+    var button = document.createElement("button");
+    if (type === "delete") {
+        button.className = "btn btn-outline-danger fa-solid fa-trash-can";
+        button.dataset.bsTarget = "#deleteButton";
+        button.dataset.bsToggle = "modal";
+    }
+    else {
+        button.className = "btn btn-outline-info fa-solid fa-pen-to-square";
+    }
+    
+    button.value = productId;
+    button.id = "button_" + productId;
+
+    return button
 }
 
 function newProduct() {
@@ -46,16 +80,70 @@ function newProduct() {
     const description = document.getElementById("descriptionText");
     const inputDresc = description.value;
 
+    // Categoría
+    const categoria = document.getElementById("category_option");
+    const inputCat = categoria.value;
+
     // Precio
     const price = document.getElementById("product_price");
     const inputPrice = price.value;
 
+    // Endpoint conection
     const Http = new XMLHttpRequest();
-    const url='http://localhost:8090/celuma-webapi/api/products/all';
+    const url='http://localhost:8090/celuma-webapi/api/products/save';
     Http.open("POST", url);
+    Http.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+    const body = JSON.stringify(
+        {
+            "name": inputName,
+            "categoryId": inputCat,
+            "content": inputDresc,
+            "instructions": null,
+            "cautions": null,
+            "active": 1
+        }
+    )
+
+    Http.onload = () => {
+        if (Http.readyState == 4 && Http.status >= 200) {
+            console.log(JSON.parse(Http.responseText));
+            location.reload();
+        }
+        else {
+            console.log("Error")
+        }
+    }
+    Http.send(body);
+
+}
+
+function getValue() {
+    var deleteBtn = document.getElementById("eliminarBtn");
+    deleteProduct(deleteBtn.value);
+
+};
+
+function deleteProduct(productId){
+    var Http = new XMLHttpRequest();
+
+    const id = productId;
+    const url = 'http://localhost:8090/celuma-webapi/api/products/delete/' + id ;
+
+    Http.open("DELETE", url);
+    Http.setRequestHeader('Content-Type', 'application/json');
+
+    Http.onreadystatechange = function() {
+        if (Http.readyState == 4) {
+            if (Http.status = 200) {
+                console.log(Http.responseText);
+                location.reload();
+            }
+            else {
+                console.log('DELETE request failed');
+            }
+        }
+    }
     Http.send();
-    
-    console.log(inputName);
-    console.log(inputDresc);
-    console.log(inputPrice);
+
 }
