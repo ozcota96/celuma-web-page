dfe33e15d3175c0a7a99405f48e7537df3377598
osvaldo orozco <osvaldoorozcorod@hmail.com>
2024-07-06 21:12:25 -0600

    Contact page is done

--- celuma-web-app/src/App.js
+++ celuma-web-app/src/App.js
@@ -14,6 +14,7 @@ import About from "./components/About/About";
 import Products from "./components/Products/Products";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -38,6 +39,7 @@ function App() {
               }/>
               <Route path="/about" element={<About/>} />
               <Route path="/products" element={<Products/>} />
+              <Route path="/contact" element={<Contact/>} />
             </Routes>
           </div>
         </div>
